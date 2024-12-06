fa7562c32d30aca47c6666af5a30e22bf707c6cf
nafiislam <1905010@ugrad.cse.buet.ac.bd>
2024-12-06 11:00:42 +0600

    g

---
+++ load_nash_results.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+import warnings
+import random
+import ast
+import utilty
+
+
+np.random.seed(42)
+random.seed(42)
+
+
+def load_dataset():
+    dataframe = pd.read_csv("nash_results.csv")
+    dataframe["player_1_strategy"] = dataframe["player_1_strategy"].apply(ast.literal_eval)
+    dataframe["player_2_strategy"] = dataframe["player_2_strategy"].apply(ast.literal_eval)
+    return dataframe
+
+df = pd.read_csv('nash_results.csv')
+print(df.head())
